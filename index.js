965b0a5a3cf700e7cf08d572c593badae03e03e8
Elizabeth Dowling <dowlinge94@gmail.com>
2022-03-21 11:06:48 -0400

    arrow fn now fn declaration so learn test runs correctly

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // Write your solution here!
 const cats = ["Milo", "Otis", "Garfield"]
 
-destructivelyAppendCat = (name) => {
+function destructivelyAppendCat(name) {
     return cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name)
 }
