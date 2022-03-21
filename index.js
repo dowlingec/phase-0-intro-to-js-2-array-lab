501b651f36d435ac83e9b876baf112e0d92f798c
Elizabeth Dowling <dowlinge94@gmail.com>
2022-03-21 11:12:59 -0400

    7 passing

--- index.js
+++ index.js
@@ -16,3 +16,11 @@ function destructivelyRemoveLastCat() {
 function destructivelyRemoveFirstCat() {
     return cats.shift()
 }
+
+function appendCat(name) {
+    return [...cats, name]
+}
+
+function prependCat(name) {
+    return [name, ...cats]
+}
